28459a524022d276c2191d96e7e979a9ba0361ee
GetanehYilma <gyletike@mum.edu>
2020-04-22 18:32:48 -0500

    models for product and cart modules added

---
+++ src/main/java/edu/miu/pm/onlineshopping/shoppingcart/model/Order.java
@@ -0,0 +1,11 @@
+package edu.miu.pm.onlineshopping.shoppingcart.model;
+
+public class Order {
+
+    private long id;
+    private int orderNumber;
+    private double totalPrice;
+    private OrderStatus orderStatus;
+    private double tax;
+    private User buyer;
+}
