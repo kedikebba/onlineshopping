fcc749e2954c281e08db89ea51b9316a9baaa0b9
zhangziyi <zhangziyiemail@gmail.com>
2020-04-28 14:43:08 -0700

    fix bug

--- src/main/java/edu/miu/pm/onlineshopping/OnlineshoppingApplication.java
+++ src/main/java/edu/miu/pm/onlineshopping/OnlineshoppingApplication.java
@@ -7,32 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineshoppingApplication implements CommandLineRunner {
-
-    @Autowired
-    private MailService mailService;
-
-    private static final String[] RECIPIENTS = {"zhangziyiemail@gmail.com"};
-    private static final String SUBJECT = "Shopping Detail";
+public class OnlineshoppingApplication {
 
 
     public static void main(String[] args) {
 		SpringApplication.run(OnlineshoppingApplication.class, args);
 	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        sendNormalTextMessage();
-    }
-
-
-    private void sendNormalTextMessage() {
-        try {
-            mailService.sendMailText(RECIPIENTS, SUBJECT, "Hello this from Online Shopping", null);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-
-    }
 
 }
