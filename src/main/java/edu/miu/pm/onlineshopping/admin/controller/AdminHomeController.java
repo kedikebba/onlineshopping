8f238de5b269f0a74a2cf007bcfc3f2b7577776c
kedikebba <kebbakedi@gmail.com>
2020-04-29 15:13:12 -0700

    renamed

---
+++ src/main/java/edu/miu/pm/onlineshopping/admin/controller/AdminHomeController.java
@@ -0,0 +1,15 @@
+package edu.miu.pm.onlineshopping.admin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class AdminHomeController {
+	
+	@RequestMapping("/home")
+	public ModelAndView homePage() {
+		ModelAndView mav=new ModelAndView("home");
+		return mav;
+	}
+}
