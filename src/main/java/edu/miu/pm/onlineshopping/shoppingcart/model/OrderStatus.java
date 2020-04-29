21fba2b7860a93182e7707b078e3738189f50496
kedikebba <kebbakedi@gmail.com>
2020-04-29 15:25:49 -0700

    added new files

--- src/main/java/edu/miu/pm/onlineshopping/shoppingcart/model/OrderStatus.java
+++ src/main/java/edu/miu/pm/onlineshopping/shoppingcart/model/OrderStatus.java
@@ -1,5 +1,5 @@
-//package edu.miu.pm.onlineshopping.shoppingcart.model;
-//
-//public enum OrderStatus {
-//    PENDING, COMPLETE
-//}
+package edu.miu.pm.onlineshopping.shoppingcart.model;
+
+public enum OrderStatus {
+    PENDING, COMPLETED, CANCELED, SHIPPED, DELIVERED;
+}
