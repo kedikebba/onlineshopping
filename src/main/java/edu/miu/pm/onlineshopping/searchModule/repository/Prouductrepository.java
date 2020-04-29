9882f5a5efc26e3d108604e393b7daf572146d0d
kedikebba <kebbakedi@gmail.com>
2020-04-29 13:55:02 -0700

    cleaning code

--- src/main/java/edu/miu/pm/onlineshopping/searchModule/repository/Prouductrepository.java
+++
@@ -1,8 +0,0 @@
-package edu.miu.pm.onlineshopping.searchModule.repository;
-
-import edu.miu.pm.onlineshopping.product.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
- public interface Prouductrepository extends JpaRepository<Product,Long> {
-
-}
